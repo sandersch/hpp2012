88fdcc9f3c5a9b272ba7d4e8167c13e613ff35c3
Charlie Sanders <sanderscharlie@gmail.com>
2012-12-10 22:42:38 -0600

    Adds CUDA matrix multiplication kernel

--- mp2.cc
+++ mp2.cc
@@ -17,6 +17,16 @@ __global__ void matrixMultiply(float * A, float * B, float * C,
                    int numBRows, int numBColumns,
                    int numCRows, int numCColumns) {
     //@@ Insert code to implement matrix multiplication here
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if ((row < numCRows) && (col < numCColumns)) {
+        float Cvalue = 0;
+        for (int k = 0; k < numAColumns; ++k) {
+            Cvalue += A[row * numAColumns + k] * B[k * numBColumns + col];
+        }
+        C[row * numCColumns + col] = Cvalue;
+    }
 }
 
 int main(int argc, char ** argv) {
